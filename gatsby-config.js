fe0d26f776ae967163f930e301e6c620842b6433
kimtoma <kimtoma@iMac-24.local>
2018-09-07 01:09:41 +0900

    initial commit with designcode

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: '디자이너를 위한 리액트 by 디자인+코드',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>안녕<br/> 세계?</h1>
+    <p>Gatsby 로 쉽게 웹사이트를 만들어보즈아아아~~</p>
+    <p>썸띵 그레잇한 웹사이트를 만들어 보자.</p>
+    <Link to="/page-2/">다음 페이지</Link>
+  </div>
+)
+
+export default IndexPage
